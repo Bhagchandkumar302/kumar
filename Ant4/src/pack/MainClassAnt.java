e85b7c57659815c4b2f97e75d91a2ee18f3e16e5
kumar <bhagchandk8@gmail.com>
2017-01-15 23:23:34 +0500

    I love Friends

---
+++ Ant4/src/pack/MainClassAnt.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class MainClassAnt {
+
+	/**
+	 * @param args kumar : love for others
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
